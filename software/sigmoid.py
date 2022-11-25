b926994a1705a0c4770ce8032623a84079b82485
Argoolja <84133779+Argoolja@users.noreply.github.com>
2022-11-26 00:41:41 +0200

    Sigmoid controller

--- software/sigmoid.py
+++ software/sigmoid.py
@@ -1,25 +1,19 @@
 import numpy as np 
 
-def sig(xcord, max_speed_X, max_speed_Y, max_speed_R, a=1):
+def sig(xcord, max_speed, a=1):
 
     frame_width = np.linspace(-424, 424, 848)
 
-    sigmoid_X = max_speed_X*2/(1 + np.exp(a*(-frame_width)))
+    sigmoid = max_speed*2/(1 + np.exp(a*(-frame_width)))
 
-    sigmoid_Y = max_speed_Y*2/(1 + np.exp(a*(-frame_width)))
+    speed = round((sigmoid[xcord] - max_speed), 2)
 
-    sigmoid_R = max_speed_R*2/(1 + np.exp(a*(-frame_width)))
-
-    speed_X = sigmoid_X[xcord] - max_speed_X
-    speed_Y = sigmoid_Y[xcord] - max_speed_Y
-    speed_R = sigmoid_R[xcord] - max_speed_R
-
-    return speed_X, speed_Y, speed_R
+    return speed
 
 
 
-xcord = 200
+xcord = 424
 a = 0.01
-max_speed=1.5
-p = sig(xcord, max_speed, a)
-print(p)
+max_speed_R=0.8
+speed_R = sig(xcord,max_speed_R, a)
+print("speedR: ", speed_R)
